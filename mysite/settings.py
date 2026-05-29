66c38399da64bb8f222ff4e681284e082b101358
Manjm1012 <manjm1012@github.com>
2026-05-28 19:27:39 -0500

    fix: rename Puzzle_1.jpg to lowercase, use CompressedStaticFilesStorage

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files (uploads: PDFs, images from tablets/phones)
 MEDIA_URL = '/media/'
