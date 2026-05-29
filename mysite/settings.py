f0fe417fd2d9d350482cba65faee6910d9d09246
Manjm1012 <manjm1012@github.com>
2026-05-28 19:24:44 -0500

    fix: add WhiteNoise to serve static files in production

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -148,6 +149,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (uploads: PDFs, images from tablets/phones)
 MEDIA_URL = '/media/'
